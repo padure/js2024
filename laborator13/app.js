9403744a4bbd37b55cd2d2b9c3bd88eb8ec9a612
padure <paduregheorghe7@gmail.com>
2024-04-09 13:12:22 +0300

    Laborator 13

---
+++ laborator13/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+const PORT = 3000;
+const URL_BOOKS = 'books.json';
+
+app.use(bodyParser.json());
+
+//Routes
+//Afisarea cartilor
+app.get('/books', (req, res) => {
+    fs.readFile(URL_BOOKS, "utf8", (err, data)=>{
+        if(err){
+            console.error(err);
+            res.status(500).send("A aparut o eroare");
+            return;
+        }
+        const books = JSON.parse(data);
+        res.json(books);
+    });
+});
+//Adaugarea unei carti
+app.post("/books/create", (req, res)=>{
+    fs.readFile(URL_BOOKS, "utf8", (err, data)=>{
+        if(err){
+            console.error(err);
+            res.status(500).send("A aparut o eroare");
+            return;
+        }
+        const books = JSON.parse(data);
+        const newBook = req.body;
+        books.push(newBook);
+        fs.writeFile(URL_BOOKS, JSON.stringify(books, null, 2), err => {
+            if(err){
+                console.error(err);
+                res.status(500).send("A aparut o eroare");
+                return;
+            }
+            res.send("Cartea a fost adaugata cu succes!");
+        });
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Serverul ruleaza la adresa: http://localhost:${PORT}`);
+});
